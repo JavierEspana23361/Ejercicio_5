53e0ae043fb6d8f66a7549c51826eedd87c9b05d
bar23354 <bar23354@uvg.edu.gt>
2023-10-06 10:33:59 -0600

    Cambios ver 1.1
    Cambio de nombre de clases para que esten similares al diagrama UML. Se agregó la logica necesaria para Main y Catalogo

--- Main.java
+++ Main.java
@@ -1,3 +1,28 @@
+/*
+ * Universidad del Valle de Guatemala
+ * POO - seccion 30
+ * Roberto Barreda 23354 y Javier España 23361
+ */
+
+/**
+ * Clase principal que contiene el método main para probar el sistema.
+ */
+
+
 public class Main {
-    
+    public static void main(String[] args) {
+        // Crear un catálogo
+        Catalogo catalogo = new Catalogo();
+
+        // Agregar materiales al catálogo (puedes adaptar esto según tus necesidades)
+        MaterialBibliografico material1 = new MaterialBibliografico("Libro1", "Editorial1", 2020, "Ficción", "Autor1", "Libro");
+        MaterialBibliografico material2 = new MaterialBibliografico("Artículo1", "Editorial2", 2019, "Ciencia", "Autor2", "Artículo");
+
+        catalogo.agregarMaterial(material1);
+        catalogo.agregarMaterial(material2);
+
+        // Mostrar el catálogo en formato APA
+        String catalogoAPA = catalogo.mostrarCatalogoAPA();
+        System.out.println("Catálogo en formato APA:\n" + catalogoAPA);
+    }
 }
--- MaterialBibliografico.java
+++ MaterialBibliografico.java
@@ -1,9 +1,18 @@
-import java.util.ArrayList;
+/*
+ * Universidad del Valle de Guatemala
+ * POO - seccion 30
+ * Roberto Barreda 23354 y Javier España 23361
+ */
+
+import java.util.*;
 import java.io.*;
 
-public class catalogManage {
-    static csvManage csv = new csvManage();
+public class MaterialBibliografico {
+    static Catalogo csv = new Catalogo();
     static String fileName = "example.csv";
+
+    public MaterialBibliografico(String string, String string2, int i, String string3, String string4, String string5) {
+    }
 
     public static void main(String[] args) {
         String tipo;
@@ -27,5 +36,29 @@ public class catalogManage {
             String cita = /* apellido + ", " + inicial + "." + " (" + año + "). " + tituloartículo + ". " + titulorevista + ", " + volumen + "(" + numrevista + "), " + numpaginicio + " - " + numpagfin + ". " + url */"";
         }
         savecita(cita);
+    }
+
+    public String getNombre() {
+        return null;
+    }
+
+    public String getGénero() {
+        return null;
+    }
+
+    public String getAutor() {
+        return null;
+    }
+
+    public int getAñoPublicacion() {
+        return 0;
+    }
+
+    public char[] toCSVString() {
+        return null;
+    }
+
+    public static MaterialBibliografico fromCSVString(String line) {
+        return null;
     }
 }
